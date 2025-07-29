a2c51c44a73a4310d452e37253a2e170d70a0297
Pravali Parvataneni <94853770+pravali70@users.noreply.github.com>
2025-07-29 15:51:37 +0530

    Add files via upload

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Program
+    {
+        public string Hello()
+        {
+            return "Welcome to C# Programming...";
+        }
+    }
+}
