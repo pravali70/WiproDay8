1d16e9caffa5c06449106dc7ec7852cdd61ab6cc
Pravali Parvataneni <94853770+pravali70@users.noreply.github.com>
2025-07-29 15:54:47 +0530

    Add files via upload

---
+++ DemoTest.cs
@@ -0,0 +1,22 @@
+﻿using Demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitDemo.Tests
+{
+    [TestFixture]
+    public class DemoTest
+    {
+        [Test]
+        public void TestDemo()
+        {
+            Program obj = new Program();
+            Assert.AreEqual("Welcome to C# Programming...", obj.Hello());
+        }
+
+
+    }
+}
