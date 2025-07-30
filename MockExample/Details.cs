8edb90ab44a5495f7460d7d29af7cb868ece19d7
Pravali Parvataneni <pravaliparvataneni@gmail.com>
2025-07-30 10:55:09 +0530

    Initial commit

---
+++ MockExample/Details.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockExample
+{
+    public class Details:IDetails
+    {
+       public string ShowCompany()
+        {
+            return "It's from Wipro Chennai..";
+        }
+        public string ShowStudent()
+        {
+            return "Hi I am pravali..";
+        }
+    }
+}
---
+++ MockExample/DetailsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace MockExample
+{
+    [TestFixture]
+    internal class DetailsTest
+    {
+        [Test]
+        public void TestShowStudent()
+        {
+            Mock<IDetails> mock = new Mock<IDetails>();
+            mock.Setup(d => d.ShowStudent()).Returns("Hi i am Madhu..");
+            Assert.AreEqual("Hi i am Madhu..", mock.Object.ShowStudent());
+        }
+
+        [Test]
+        public void TestShowCompany()
+        {
+            Mock<IDetails> obj = new Mock<IDetails>();
+            obj.Setup(d => d.ShowCompany()).Returns("Its from wipro Bangalore..");
+            Assert.AreEqual("Its from wipro Bangalore..", obj.Object.ShowCompany());
+        }
+    }
+}
