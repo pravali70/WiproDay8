65ff405c120f11d2d286dd94d7fb1d142a47b2a2
Pravali Parvataneni <pravaliparvataneni@gmail.com>
2025-07-30 11:32:36 +0530

    Initial commit

---
+++ CalculatorTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CalculatorTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
